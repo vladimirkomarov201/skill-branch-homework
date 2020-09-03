42f7703037ec6df695520feb2a0cc4546e55cf07
Vladimir <vkomarov@baccasoft.ru>
2020-09-03 10:24:17 +0400

    переделал делегат

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/ViewModelDelegate.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/base/ViewModelDelegate.kt
@@ -2,18 +2,21 @@ package ru.skillbranch.skillarticles.viewmodels.base
 
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModel
-import ru.skillbranch.skillarticles.viewmodels.ArticleViewModel
+import androidx.lifecycle.ViewModelProviders
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 class ViewModelDelegate<T : ViewModel>(private val clazz: Class<T>, private val arg: Any?) :
     ReadOnlyProperty<FragmentActivity, T> {
 
+    private lateinit var value: T
+
     override fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T {
-        if (clazz.isAssignableFrom(ArticleViewModel::class.java)) {
-            return ArticleViewModel(arg as String) as T
+        if (!::value.isInitialized) value = when(arg){
+            null -> ViewModelProviders.of(thisRef).get(clazz)
+            else -> ViewModelProviders.of(thisRef, ViewModelFactory(arg)).get(clazz)
         }
-        throw IllegalArgumentException("Unknown ViewModel class")
+        return value
     }
 
 }
