1fe2291d011962ee452306d7f491f1d8e404ef20
Vladimir <vkomarov@baccasoft.ru>
2021-06-15 07:38:36 +0400

    удалил лишнее

--- app/src/main/java/ru/skillbranch/skillarticles/ui/auth/AuthFragment.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/auth/AuthFragment.kt
@@ -3,7 +3,6 @@ package ru.skillbranch.skillarticles.ui.auth
 import android.text.Spannable
 import androidx.core.text.set
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_auth.*
 import ru.skillbranch.skillarticles.R
@@ -19,7 +18,7 @@ class AuthFragment : BaseFragment<AuthViewModel>() {
 
     override fun setupViews() {
         tv_privacy.setOnClickListener {
-            findNavController().navigate(R.id.page_privacy_policy)
+//            findNavController().navigate(R.id.page_privacy_policy)
         }
         btn_login.setOnClickListener {
             viewModel.handleLogin(
--- app/src/main/java/ru/skillbranch/skillarticles/ui/auth/PrivacyPolicyFragment.kt
+++
@@ -1,8 +0,0 @@
-package ru.skillbranch.skillarticles.ui.auth
-
-import androidx.fragment.app.Fragment
-import ru.skillbranch.skillarticles.R
-
-class PrivacyPolicyFragment : Fragment(R.layout.fragment_privacy_policy) {
-
-}
